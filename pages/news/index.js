b83c777a40983d34ee841757536a0f50ad4a3ffd
Palak-Srivastava4 <palak.sri.dps@gmail.com>
2024-07-03 19:27:19 +0530

    checking code

--- pages/news/index.js
+++
@@ -1,8 +0,0 @@
-import News from "@/app/components/News/News";
-import React from "react";
-
-const news = () => {
-  return <News />;
-};
-
-export default news;
--- pages/singleNews/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import SingleNews from "@/app/components/News/SingleNews";
-
-const singleNews = () => {
-  return <SingleNews/>
-}
-
-export default singleNews;
